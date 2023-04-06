707c2b72beee1e11759ce5689296c8060c8ab12a
inesmaria20101 <130046955+inesmaria20101@users.noreply.github.com>
2023-04-06 10:29:16 +0100

    Add files via upload

---
+++ behaviour.js
@@ -0,0 +1,6 @@
+let messageButton=document.querySelector("#click-button");
+messageButton.addEventListener("click",showMessage);
+function showMessage(event){
+let message="Obrigada por virem,fico muito feliz";
+alert(message);
+}
